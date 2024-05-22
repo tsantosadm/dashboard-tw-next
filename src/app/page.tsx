68c44fc9a7f7c1cb674998bfffa131df72386b28
Talisson Santos <tsantosadm@gmail.com>
2024-05-22 19:52:43 -0300

    abas navigation radix tabs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
+import SettingsTabs from '@/components/SettingsTabs';
+import * as Tabs from '@radix-ui/react-tabs'
+
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <>
+      <h1 className="text-3xl font-medium text-zinc-900">Settings</h1>
+
+     <SettingsTabs />
+    </>
   );
 }
