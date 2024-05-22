c029c533e71392d78e353c59286199c4899f1342
Talisson Santos <tsantosadm@gmail.com>
2024-05-22 19:59:41 -0300

    motino tab move

--- src/components/SettingsTabs/TabItem.tsx
+++ src/components/SettingsTabs/TabItem.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import * as Tabs from '@radix-ui/react-tabs'
+import { motion } from 'framer-motion'
 
 export interface TabItemProps {
     value: string;
@@ -13,7 +14,7 @@ export function TabItem({value, title, isSelected = false}: TabItemProps){
         <Tabs.Trigger value={value} className='relative px-1 pb-4 text-sm font-medium text-zinc-500 hover:text-violet-700 data-[state=active]:text-violet-700'>
             <span>{title}</span>
             {isSelected && (
-                <div className='absolute -bottom-px left-0 right-0 h-0.5 bg-violet-700' />
+                <motion.div layoutId='tabId' className='absolute -bottom-px left-0 right-0 h-0.5 bg-violet-700' />
             )}
         </Tabs.Trigger>
     )
