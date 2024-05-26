8ae4c1ec02392800bfe0211009fedb036cb0086b
Talisson Santos <tsantosadm@gmail.com>
2024-05-26 13:32:07 -0300

    fields form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import SettingsTabs from '@/components/SettingsTabs';
-import * as Tabs from '@radix-ui/react-tabs'
+import * as Input from "@/components/Input"
+import { Mail } from 'lucide-react';
 
 export default function Home() {
   return (
@@ -17,16 +18,101 @@ export default function Home() {
             </span>
           </div>
           <div className='flex items-center gap-2'>
-            <button type='button' 
-                className='rounded-lg border border-zinc-300 px-4 py-2 text-sm font-semibold text-zinc-700 shadow-sm hover:bg-zinc-50'>
-                Cancel
-              </button>
-            <button type='submit' form='settings' className='rounded-lg bg-violet-600 px-4 py-2 font-semibold text-white shadow-ms hover:bg-violet-700'>Save</button>
+            <button type='button'
+              className='rounded-lg border border-zinc-300 px-4 py-2 text-sm font-semibold text-zinc-700 shadow-sm hover:bg-zinc-50'>
+              Cancel
+            </button>
+            <button type='submit' form='settings' className='rounded-lg bg-violet-600 px-4 py-2 font-semibold text-white shadow-ms hover:bg-violet-700'>
+              Save
+            </button>
           </div>
         </div>
 
-        <form id='settings' className='mt-6 flex flex-col w-full' >
+        <form id='settings' className='mt-6 flex flex-col w-full gap-5 divide-y divide-zinc-200' >
+          <div className='grid grid-cols-form gap-3'>
+            <label htmlFor='firstName' className='text-sm font-medium text-zinc-700'>
+              Name
+            </label>
+            <div className='grid gap-6 grid-cols-2'>
+              <Input.InputRoot>
+                <Input.InputControl id='firstName' defaultValue='Neo' />
+              </Input.InputRoot>
 
+              <Input.InputRoot>
+                <Input.InputControl defaultValue='Tecnologia' />
+              </Input.InputRoot>
+            </div>
+          </div>
+
+          <div className='grid grid-cols-form gap-3 pt-5'>
+            <label htmlFor='email' className='text-sm font-medium text-zinc-700'>
+              Email address
+            </label>
+            <Input.InputRoot>
+              <Input.InputPrefix>
+                <Mail />
+              </Input.InputPrefix>
+              <Input.InputControl id='email' type='email' defaultValue='talisson@neotecnologia.com.br' />
+            </Input.InputRoot>
+          </div>
+
+          <div className='grid grid-cols-form gap-3 pt-5'>
+            <label htmlFor='email' className='text-sm font-medium text-zinc-700'>
+              Your photo
+              <span className='mt-0.5 block text-sm font-normal text-zinc-500'>This will be displayed on your profile</span>
+            </label>
+            <div></div>
+          </div>
+
+          <div className='grid grid-cols-form gap-3 pt-5'>
+            <label htmlFor='role' className='text-sm font-medium text-zinc-700'>
+              Role
+            </label>
+            <Input.InputRoot>
+              <Input.InputControl id='role' defaultValue='CTO' />
+            </Input.InputRoot>
+          </div>
+
+          <div className='grid grid-cols-form gap-3 pt-5'>
+            <label htmlFor='country' className='text-sm font-medium text-zinc-500'>
+              Country
+            </label>
+            <div></div>
+          </div>
+
+          <div className='grid grid-cols-form gap-3 pt-5'>
+            <label htmlFor='timezone' className='text-sm font-medium text-zinc-500'>
+              Timezone
+            </label>
+          </div>
+
+          <div className='grid grid-cols-form gap-3 pt-5'>
+            <label htmlFor='bio' className='text-sm font-medium text-zinc-700'>
+              Bio
+              <span className='mt-0.5 block text-sm font-normal text-zinc-500'>
+                Write a short introduction
+              </span>
+            </label>
+            <div></div>
+          </div>
+
+          <div className='grid grid-cols-form gap-3 pt-5'>
+            <label htmlFor='projects' className='text-sm font-medium text-zinc-700'>
+              Portfolio projects
+              <span>
+                Share a few snippets of your work.
+              </span>
+            </label>
+          </div>
+
+          <div className='flex items-center justify-end gap-2 pt-5'>
+            <button type='button' className='rounded-lg border-zinc-300 px-4 py-2 text-sm font-semibold text-zinc-700 shadow-sm hover:bg-zinc-50'>
+              Cancel
+            </button>
+            <button type='button' className='rounded-lg bg-violet-600 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-violet-700'>
+              Save
+            </button>
+          </div>
         </form>
       </div>
     </>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,7 +8,8 @@ const config: Config = {
     extend: {
       gridTemplateColumns: {
         dashboard: 'minmax(18rem, 20rem) 1fr',
-        profile: 'max-content 1fr min-content'
+        profile: 'max-content 1fr min-content',
+        form: 'minmax(7.5rem, 17.5rem) minmax(25rem, 1fr) minmax(0, 15rem)'
       },
       colors: {
        primary: '#8257e6'
