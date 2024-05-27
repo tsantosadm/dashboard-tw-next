d6485293cbf4c3e4ea980c5482bb874a24a4ea1e
Talisson Santos <tsantosadm@gmail.com>
2024-05-27 15:00:50 -0300

    upload files avatar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import SettingsTabs from '@/components/SettingsTabs';
 import * as Input from "@/components/Input"
-import { Mail } from 'lucide-react';
+import { Mail, UploadCloud, User } from 'lucide-react';
 
 export default function Home() {
   return (
@@ -57,11 +57,31 @@ export default function Home() {
           </div>
 
           <div className='grid grid-cols-form gap-3 pt-5'>
-            <label htmlFor='email' className='text-sm font-medium text-zinc-700'>
+            <label htmlFor='photo' className='text-sm font-medium text-zinc-700'>
               Your photo
               <span className='mt-0.5 block text-sm font-normal text-zinc-500'>This will be displayed on your profile</span>
             </label>
-            <div></div>
+            <div className='flex item-start gap-5'>
+              <div className='flex h-16 w-16 items-center justify-center rounded-full bg-violet-50'>
+                <User className='h-8 w-8 text-violet-500' />
+              </div>
+              <label htmlFor='photo' className='flex-1 cursor-pointer flex flex-col items-center gap-3 rounded-lg border border-zinc-300 px-6 py-4 text-center text-zinc-500 shadow-sm hover:border-violet-200 hover:bg-violet-25'>
+                <div className='group rounded-full border-6 border-zinc-50 bg-zinc-100 p-2 group-hover:border-violet-50 group-hover:bg-violet-100'>
+                  <UploadCloud className='h-5 w-5 text-zinc-600 group-hover:text-violet-700' />
+                </div>
+
+                <div className='flex flex-col items-center gap-1'>
+                  <span className='text-sm'>
+                    <span className='font-semibold text-violet-700'>
+                      Click to upload
+                    </span>{' '}
+                    or drag and drop
+                  </span>
+                  <span className='text-xs'>SVG, PNG, JPG ou GIF (max. 800x400x)</span>
+                </div>
+              </label>
+              <input type='file' className='sr-only' id='photo' />
+            </div>
           </div>
 
           <div className='grid grid-cols-form gap-3 pt-5'>
